2c0bdcb0502b48c5166c62c759431434e987d16a
Facundo Javier Gelatti <javiergelatti@gmail.com>
2019-03-05 21:55:58 -0300

    Allow more than one editor to be present at a time

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
 const { variable, application, lambda, hole } = require('./src/ast')
 const { toHtml } = require('./src/toHtml')
 
-let expression = application(application(lambda('x', lambda('y', hole())), variable('a')), variable('b'))
-let contenedor = document.getElementById("contenedor").querySelector(".lambda-calculus-expression > .expression")
-
-let undoQueue = []
-let redoQueue = []
-
 function on(event, element, handler) {
     element.addEventListener(event, event => {
         handler()
@@ -18,116 +12,138 @@ function onClick(element, handler) {
     return on('click', element, handler)
 }
 
-function updateExpression(newExpression) {
-    if (expression !== newExpression) {
-        undoQueue.push(expression)
-        redoQueue = []
-        undoButton.disabled = false
-        redoButton.disabled = true
-        expression = newExpression
-        render()
+class Editor {
+    constructor(container, initialExpression = hole()) {
+        this.expression = initialExpression
+        this.undoQueue = []
+        this.redoQueue = []
+        this.undoButton = container.querySelector("button[name='undo']")
+        this.redoButton = container.querySelector("button[name='redo']")
+        this.container = container
+        this.expressionContainer = container.querySelector(".expression")
+    }
+
+    updateExpression(newExpression) {
+        if (this.expression !== newExpression) {
+            this.undoQueue.push(this.expression)
+            this.redoQueue = []
+            this.undoButton.disabled = false
+            this.redoButton.disabled = true
+            this.expression = newExpression
+            this.render()
+        }
+    }
+
+    setUpActionsOn(selector, actions) {
+        this.expressionContainer.querySelectorAll(selector).forEach(element => {
+            on('click', element, () => {
+                if (element.classList.contains('active')) {
+                    this.deactivateAllNodes()
+                } else {
+                    this.deactivateAllNodes()
+                    element.classList.add('active')
+                }
+            })
+            on('mouseover', element, () => {
+                element.classList.add('hovered')
+            })
+            on('mouseout', element, () => {
+                element.classList.remove('hovered')
+            })
+
+            for (const action in actions) {
+                onClick(element.querySelector('.' + action), () => {
+                    let newExpression = actions[action](element.astNode, this.expression)
+                    this.updateExpression(newExpression)
+                })
+            }
+        })
+    }
+
+    deactivateAllNodes() {
+        this.expressionContainer.querySelectorAll('.active').
+            forEach(a => a.classList.remove('active'))
+    }
+
+    render() {
+        this.expressionContainer.innerHTML = ''
+        this.expressionContainer.appendChild(toHtml(this.expression))
+
+        this.setUpActionsOn('.hole', {
+            'insert-variable': (selectedHole, expression) => {
+                let variableName = prompt('Variable name?')
+                if (variableName) {
+                    return expression.replace(selectedHole, variable(variableName))
+                } else {
+                    return expression
+                }
+            },
+            'insert-abstraction': (selectedHole, expression) => {
+                let variableName = prompt('Variable name?')
+                if (variableName) {
+                    return expression.replace(selectedHole, lambda(variableName, hole()))
+                } else {
+                    return expression
+                }
+            },
+            'insert-application': (selectedHole, expression) => {
+                return expression.replace(selectedHole, application(hole(), hole()))
+            }
+        })
+
+        this.setUpActionsOn('.abstraction, .application, *:not(.parameter) > .variable', {
+            'delete': (node, expression) => {
+                return expression.replace(node, hole())
+            },
+            'wrap-lambda': (node, expression) => {
+                let variableName = prompt('Variable name?')
+                if (variableName) {
+                    return expression.replace(node, lambda(variableName, node))
+                } else {
+                    return expression
+                }
+            },
+            'wrap-application-argument': (node, expression) => {
+                return expression.replace(node, application(hole(), node))
+            },
+            'wrap-application-function': (node, expression) => {
+                return expression.replace(node, application(node, hole()))
+            },
+        })
+    }
+
+    init() {
+        document.body.addEventListener('click', () => this.deactivateAllNodes())
+
+        onClick(this.container.querySelector("button[name='evaluate']"), () => {
+            this.updateExpression(this.expression.fullBetaReduce())
+        })
+
+        onClick(this.undoButton, () => {
+            this.redoQueue.push(this.expression)
+            this.expression = this.undoQueue.pop()
+            this.undoButton.disabled = this.undoQueue.length === 0
+            this.redoButton.disabled = this.redoQueue.length === 0
+            this.render()
+        })
+
+        onClick(this.redoButton, () => {
+            this.undoQueue.push(this.expression)
+            this.expression = this.redoQueue.pop()
+            this.undoButton.disabled = this.undoQueue.length === 0
+            this.redoButton.disabled = this.redoQueue.length === 0
+            this.render()
+        })
+
+        this.render()
     }
 }
 
-function setUpActionsOn(selector, actions) {
-    contenedor.querySelectorAll(selector).forEach(element => {
-        on('click', element, () => {
-            if (element.classList.contains('active')) {
-                deactivateAllNodes()
-            } else {
-                deactivateAllNodes()
-                element.classList.add('active')
-            }
-        })
-        on('mouseover', element, () => {
-            element.classList.add('hovered')
-        })
-        on('mouseout', element, () => {
-            element.classList.remove('hovered')
-        })
-
-        for (const action in actions) {
-            onClick(element.querySelector('.' + action), () => {
-                let newExpression = actions[action](element.astNode, expression)
-                updateExpression(newExpression)
-            })
-        }
-    })
-}
-
-function deactivateAllNodes() {
-    contenedor.querySelectorAll('.active').forEach(a => a.classList.remove('active'))
-}
-
-document.body.addEventListener('click', deactivateAllNodes)
-
-let render = () => {
-    contenedor.innerHTML = ''
-    contenedor.appendChild(toHtml(expression))
-
-    setUpActionsOn('.hole', {
-        'insert-variable': (selectedHole, expression) => {
-            let variableName = prompt('Variable name?')
-            if (variableName) {
-                return expression.replace(selectedHole, variable(variableName))
-            } else {
-                return expression
-            }
-        },
-        'insert-abstraction': (selectedHole, expression) => {
-            let variableName = prompt('Variable name?')
-            if (variableName) {
-                return expression.replace(selectedHole, lambda(variableName, hole()))
-            } else {
-                return expression
-            }
-        },
-        'insert-application': (selectedHole, expression) => {
-            return expression.replace(selectedHole, application(hole(), hole()))
-        }
+document.
+    getElementById("contenedor").
+    querySelectorAll(".lambda-calculus-expression").
+    forEach(container => {
+        let expression = application(application(lambda('x', lambda('y', hole())), variable('a')), variable('b'))
+        new Editor(container, expression).init()
     })
 
-    setUpActionsOn('.abstraction, .application, *:not(.parameter) > .variable', {
-        'delete': (node, expression) => {
-            return expression.replace(node, hole())
-        },
-        'wrap-lambda': (node, expression) => {
-            let variableName = prompt('Variable name?')
-            if (variableName) {
-                return expression.replace(node, lambda(variableName, node))
-            } else {
-                return expression
-            }
-        },
-        'wrap-application-argument': (node, expression) => {
-            return expression.replace(node, application(hole(), node))
-        },
-        'wrap-application-function': (node, expression) => {
-            return expression.replace(node, application(node, hole()))
-        },
-    })
-}
-
-onClick(document.getElementById("contenedor").querySelector("button[name='evaluate']"), () => {
-    updateExpression(expression.fullBetaReduce())
-})
-
-let undoButton = document.getElementById("contenedor").querySelector("button[name='undo']")
-onClick(undoButton, () => {
-    redoQueue.push(expression)
-    redoButton.disabled = false
-    expression = undoQueue.pop()
-    undoButton.disabled = !(undoQueue.length > 0)
-    render()
-})
-
-let redoButton = document.getElementById("contenedor").querySelector("button[name='redo']")
-onClick(redoButton, () => {
-    undoQueue.push(expression)
-    undoButton.disabled = false
-    expression = redoQueue.pop()
-    redoButton.disabled = !(redoQueue.length > 0)
-    render()
-})
-
-render()
--- src/content.md
+++ src/content.md
@@ -3,7 +3,8 @@
 # Titulo
 
 <div id="contenedor">
-<span class="lambda-calculus-expression">
+
+<div class="lambda-calculus-expression">
 <span class="expression">
 </span>
 <span class="actions">
@@ -11,7 +12,20 @@
 <button name="redo" disabled>Rehacer</button>
 <button name="evaluate">¡Evaluar!</button>
 </span>
+</div>
+
+<br>
+
+<div class="lambda-calculus-expression">
+<span class="expression">
 </span>
+<span class="actions">
+<button name="undo" disabled>Deshacer</button>
+<button name="redo" disabled>Rehacer</button>
+<button name="evaluate">¡Evaluar!</button>
+</span>
+</div>
+
 </div>
 
 <script src="bundle.js"></script>
